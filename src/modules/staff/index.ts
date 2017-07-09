80c8207d516d16bbe696740b5498dc727d2bc8c8
Alexandre Gaudencio <shahor@shahor.fr>
2017-07-09 21:59:56 +0200

    Ask the question to the user

--- src/modules/staff/index.ts
+++ src/modules/staff/index.ts
@@ -1,6 +1,41 @@
-import { getRandomNote } from "./constants"
+import { white } from "chalk"
+import { exit } from "process"
+import { get, start, stop } from "prompt"
+import { getRandomNote, Note } from "./constants"
 import { draw } from "./renderer"
 
+function handleUserInput(note: string, pickedNote: Note) {
+	stop()
+	note = note.substr(0, 1)
+	const picked: string = Note[pickedNote].substr(0, 1)
+
+	if (note === picked) {
+		console.log(`✅  well done!`)
+		exit(0)
+	}
+	console.log(`❌  What a noob :D`)
+	exit(1)
+}
+
 export function ask(): void {
-	draw(getRandomNote())
+	const note: Note = getRandomNote()
+	draw(note)
+
+	start()
+	get(
+		{
+			properties: {
+				note: {
+					description: white("Enter the note you read"),
+					message: "A note is a letter between A and G (inclusive)",
+					pattern: /[a-gA-G]\d{0,1}/,
+					required: true,
+				},
+			},
+		},
+		(_, result: { note: string }) => {
+			stop()
+			handleUserInput(result.note.toUpperCase(), note)
+		},
+	)
 }
---
+++ src/typings/prompt.d.ts
@@ -0,0 +1,56 @@
+// Type definitions for Prompt.js 0.2.14
+// Project: https://github.com/flatiron/prompt
+
+declare module "prompt" {
+	type properties = string[] | p.PromptSchema | p.PromptPropertyOptions[]
+
+	namespace p {
+		interface PromptSchema {
+			properties: PromptProperties
+		}
+
+		interface PromptProperties {
+			[propName: string]: PromptPropertyOptions
+		}
+
+		interface PromptPropertyOptions {
+			pattern?: RegExp
+			message?: string
+			required?: boolean
+			hidden?: boolean
+			description?: string
+			type?: string
+			default?: string
+			before?: (value: any) => any
+			conform?: (result: any) => boolean
+			name?: string
+		}
+
+		export function start(): void
+		export function stop(): void
+
+		export function get(
+			properties: properties,
+			callback: (err: Error, result: string) => void,
+		): void
+
+		export function get<T>(
+			properties: properties,
+			callback: (err: Error, result: T) => void,
+		): void
+
+		export function addProperties(
+			obj: any,
+			properties: properties,
+			callback: (err: Error) => void,
+		): void
+
+		export function history(propertyName: string): any
+
+		export var override: any
+		export var colors: boolean
+		export var message: string
+		export var delimiter: string
+	}
+	export = p
+}
